d3d8b05e630e9ab1166dfc9e493a5a474b17c554
Evelina Frännhag <evelinafrannhag@gmail.com>
2025-06-14 14:33:05 +0200

    Move definition of datatypes to its' own file, and integrate changes to error handling

---
+++ src/datatypes.rs
@@ -0,0 +1,98 @@
+use std::{collections::HashMap, fmt, rc::Rc};
+
+use crate::primitives::{add, eq, gr, inv, mul, neg};
+
+pub type Num = f64;
+pub type PosNum = usize;
+pub type Pos = Vec<PosNum>;
+
+/// The basic unit of code in the language. Any valid piece of Linsl code is an expression.
+#[derive(Debug, Clone)]
+pub enum LinslExpr {
+    Bool(bool),
+    /// A lambda function, in the spirit of lambda calculus.
+    Closure(Rc<LinslExpr>, Rc<LinslExpr>),
+    List(Vec<LinslExpr>),
+    Number(Num),
+    /// A built in transformation of expressions. These have deliberately been kept as few as
+    /// possible; there are just enough of them to allow other functions that are desirable to be
+    /// defined in Linsl.
+    Primitive(fn(&[LinslExpr]) -> Result<LinslExpr, LinslErr>),
+    Symbol(String),
+}
+
+impl fmt::Display for LinslExpr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let str = match self {
+            LinslExpr::Bool(b)          => if *b {"#t".to_string()} else {"#f".to_string()}
+            LinslExpr::Closure(ps, bd)  => format!("(lambda {}, {})", ps, bd),
+            LinslExpr::Primitive(_)     => "Primitive operator".to_string(),
+            LinslExpr::List(xs)         => {
+                let strs : Vec<String> = xs
+                    .iter()
+                    .map(|x| x.to_string())
+                    .collect();
+                format!("({})", strs.join(" "))
+            }
+            LinslExpr::Number(v)        => v.to_string(),
+            LinslExpr::Symbol(s)        => s.clone(),
+        };
+
+        write!(f, "{}", str)
+    }
+}
+
+/// Errors that can be encountered when parsing or evaluating code.
+#[derive(Debug)]
+pub enum LinslErr {
+    InternalError(String),
+    SyntaxError(String, Pos),
+    /// Created if the number of opening parens is not the same as closing parens.
+    /// Returns (number of '(', number of ')')
+    UnbalancedParens(PosNum, PosNum),
+}
+
+impl fmt::Display for LinslErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            LinslErr::InternalError(s) => s.clone(),
+            LinslErr::SyntaxError(s, p) => {
+                let mut poss: Vec<String> = p.iter()
+                    .map(|c| c.to_string())
+                    .collect();
+                poss.pop();
+                poss.reverse();
+                let vec = format!("({})", poss.join(", "));
+                format!("Syntax error at {}: {}", vec, s)
+            },
+            LinslErr::UnbalancedParens(v1, v2) => format!("Unbalanced Parenthesis ({}, {})", v1, v2),
+        };
+
+        write!(f, "{}", str)
+    }
+}
+
+/// The current bindings between symbol names and code.
+#[derive(Debug, Clone)]
+pub struct LinslEnv<'a> {
+    pub inner: HashMap<String, LinslExpr>,
+    pub outer: Option<&'a LinslEnv<'a>>,
+}
+
+impl LinslEnv<'_> {
+    /// The environment when starting the interpreter, i.e. holding only the primitives.
+    pub fn default<'a>() -> LinslEnv<'a> {
+        let mut env = HashMap::new();
+
+        env.insert("+".to_string(), LinslExpr::Primitive(add));
+        env.insert("neg".to_string(), LinslExpr::Primitive(neg));
+        env.insert("*".to_string(), LinslExpr::Primitive(mul));
+        env.insert("inv".to_string(), LinslExpr::Primitive(inv));
+        env.insert("=".to_string(), LinslExpr::Primitive(eq));
+        env.insert(">".to_string(), LinslExpr::Primitive(gr));
+        LinslEnv { 
+            inner: env,
+            outer: None,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,24 @@
 //! A simple interpreter for a lisp/scheme like language
 
-use core::fmt;
-use std::{collections::HashMap, io::{self, Write}, rc::Rc};
+use std::io::{self, Write};
 
+mod datatypes;
+mod evaluation;
 mod primitives;
 mod parsing;
-mod evaluation;
 
 use evaluation::evaluate;
 use parsing::{parse, tokenize};
-use primitives::{add, eq, gr, inv, mul, neg};
+use datatypes::{LinslEnv, LinslErr, LinslExpr};
 
-type Num = f64;
-
-/// The basic unit of code in the language. Any valid piece of Linsl code is an expression.
-#[derive(Debug, Clone)]
-enum LinslExpr {
-    Bool(bool),
-    /// A lambda function, in the spirit of lambda calculus.
-    Closure(Rc<LinslExpr>, Rc<LinslExpr>),
-    List(Vec<LinslExpr>),
-    Number(Num),
-    /// A built in transformation of expressions. These have deliberately been kept as few as
-    /// possible; there are just enough of them to allow other functions that are desirable to be
-    /// defined in Linsl.
-    Primitive(fn(&[LinslExpr]) -> Result<LinslExpr, LinslErr>),
-    Symbol(String),
-}
-
-impl fmt::Display for LinslExpr {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let str = match self {
-            LinslExpr::Bool(b)          => if *b {"#t".to_string()} else {"#f".to_string()}
-            LinslExpr::Closure(ps, bd)  => format!("(lambda {}, {})", ps, bd),
-            LinslExpr::Primitive(_)     => "Primitive operator".to_string(),
-            LinslExpr::List(xs)         => {
-                let strs : Vec<String> = xs
-                    .iter()
-                    .map(|x| x.to_string())
-                    .collect();
-                format!("({})", strs.join(" "))
-            }
-            LinslExpr::Number(v)        => v.to_string(),
-            LinslExpr::Symbol(s)        => s.clone(),
-        };
-
-        write!(f, "{}", str)
-    }
-}
-
-/// Errors that can be encountered when parsing or evaluating code.
-#[derive(Debug)]
-enum LinslErr {
-    InternalError(String),
-    ListError(String),
-    PrimitivesError(String),
-    SyntaxError(u32, u32),
-    UnbalancedParens(u32, u32),
-}
-
-impl fmt::Display for LinslErr {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let str = match self {
-            LinslErr::InternalError(s) => s.clone(),
-            LinslErr::ListError(s) => s.clone(),
-            LinslErr::PrimitivesError(s) => s.clone(),
-            LinslErr::SyntaxError(v1, v2) => format!("Syntax error at ({}, {})", v1, v2),
-            LinslErr::UnbalancedParens(v1, v2) => format!("Unbalanced Parenthesis at ({}, {})", v1, v2),
-        };
-
-        write!(f, "{}", str)
-    }
-}
-
-/// The current bindings between symbol names and code.
-#[derive(Debug, Clone)]
-struct LinslEnv<'a> {
-    inner: HashMap<String, LinslExpr>,
-    outer: Option<&'a LinslEnv<'a>>,
-}
-
-impl LinslEnv<'_> {
-    /// The environment when starting the interpreter, i.e. holding only the primitives.
-    fn default<'a>() -> LinslEnv<'a> {
-        let mut env = HashMap::new();
-
-        env.insert("+".to_string(), LinslExpr::Primitive(add));
-        env.insert("neg".to_string(), LinslExpr::Primitive(neg));
-        env.insert("*".to_string(), LinslExpr::Primitive(mul));
-        env.insert("inv".to_string(), LinslExpr::Primitive(inv));
-        env.insert("=".to_string(), LinslExpr::Primitive(eq));
-        env.insert(">".to_string(), LinslExpr::Primitive(gr));
-        LinslEnv { 
-            inner: env,
-            outer: None,
-        }
-    }
-}
 
 fn parse_eval(expr: String, env: &mut LinslEnv) -> Result<LinslExpr, LinslErr> {
-    let (parse_res, _) = parse(&tokenize(expr, 0))?;
-    let res = evaluate(&parse_res, env)?;
+    let (parse_res, rest) = parse(&tokenize(expr)?, 0)?;
+    if !rest.is_empty() {
+        return Err(LinslErr::SyntaxError("Unexpected characters at end.".to_string(), Vec::new()));
+    };
+
+    let res = evaluate(&parse_res, 0, env)?;
     Ok(res)
 }
 
